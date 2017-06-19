9ee0421c1cda91def1e370b82c68654a0113ef86
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-06-19 17:48:04 +0200

    remove wrong dependency on glut

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 mkdir -Force build
 cd build
 cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static"
-cmake --build . --target install
+cmake --build . --target install --config Release
+#cmake --build . --target install --config Debug
 cd ..
