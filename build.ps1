c1cccbcf437c240a7b0c9716eae1873ebaacad66
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-08-25 18:17:14 +0200

    fixes for VS 15.3

--- build.ps1
+++ build.ps1
@@ -1,6 +1,10 @@
+#mkdir -Force build_debug
+#cd build_debug
+#cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static"
+#cmake --build . --config Debug
+#cd ..
 mkdir -Force build
 cd build
-cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static"
+cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release"
 cmake --build . --target install --config Release
-#cmake --build . --target install --config Debug
 cd ..
